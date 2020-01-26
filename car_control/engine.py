5ef99a92880a1d036efd1302f02b176f46d1aabb
romlm <romain103paris@gmail.com>
2020-01-26 19:38:32 +0100

    change pins args to tuple

--- car_control/engine.py
+++ car_control/engine.py
@@ -2,17 +2,14 @@ import RPi.GPIO as GPIO
 
 class Engine:
 
-        def __init__(self, In1, In2, En, mode=GPIO.BCM):
-                self._In1 = In1
-                self._In2 = In2
-                self._En = En
+        def __init__(self, pins):
+                self._pins=pins
 
-                GPIO.setmode(mode)
-                GPIO.setup(self._In1, GPIO.OUT)
-                GPIO.setup(self._In2, GPIO.OUT)
-                GPIO.setup(self._En, GPIO.OUT)
+                GPIO.setup(self._pins[0], GPIO.OUT)
+                GPIO.setup(self._pins[1], GPIO.OUT)
+                GPIO.setup(self._pins[2], GPIO.OUT)
 
-                self._pwn = GPIO.PWM(self._En, 100)
+                self._pwn = GPIO.PWM(self._pins[2], 100)
                 self._pwm.start(100)
 
         def set_power(self, pwm=100.0):
@@ -25,16 +22,16 @@ class Engine:
 
         def reset(self):
                 self._pwm.stop()
-                GPIO.output(self._In1, GPIO.LOW)
-                GPIO.output(self._In2, GPIO.LOW)
-                GPIO.output(self._En, GPIO.LOW)
+                GPIO.output(self._pins[0], GPIO.LOW)
+                GPIO.output(self._pins[1], GPIO.LOW)
+                GPIO.output(self._pins[2], GPIO.LOW)
 
         def go_forward(self):
-                GPIO.output(self._In1, GPIO.HIGH)
-                GPIO.output(self._In2, GPIO.LOW)
-                GPIO.output(self._En, GPIO.HIGH)
+                GPIO.output(self._pins[0], GPIO.HIGH)
+                GPIO.output(self._pins[1], GPIO.LOW)
+                GPIO.output(self._pins[2], GPIO.HIGH)
         
         def go_backward(self):
-                GPIO.output(self._In1, GPIO.LOW)
-                GPIO.output(self._In2, GPIO.HIGH)
-                GPIO.output(self._En, GPIO.HIGH)
+                GPIO.output(self._pins[0], GPIO.LOW)
+                GPIO.output(self._pins[1], GPIO.HIGH)
+                GPIO.output(self._pins[2], GPIO.HIGH)
